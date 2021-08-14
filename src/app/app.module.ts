2fdbb1167c7cd86d0f10db17eda981b534646176
alegarciy <ale.garciy@gmail.com>
2021-08-13 21:22:02 -0600

    Added some pre-made compoents from Angular Materia and Boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginDashboardComponent } from './Login/login-dashboard/login-dashboard.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+/* Angular Material */
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginDashboardComponent
-  ],
+  declarations: [AppComponent, LoginDashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatListModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
